bfb77740a15b68481b972c5bdeacbe0fdf1331c7
vrundanawal <nawal.vrunda@gmail.com>
2022-10-24 16:13:34 +0100

    add initial test and Checkbox disables button on first click and enables on second click

---
+++ src/pages/summary/test/SummaryForm.test.js
@@ -0,0 +1,28 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import SummaryForm from "../SummaryForm";
+
+test("Initial Conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox disables button on first click and enables on second click", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
